17ea0ef938a32bccbebd732355a57c1d978f9a3f
buckley <none@none>
2012-12-13 16:57:54 +0000

    Improvements to analytic example (discarding typedefs, code and naming style tweaks, and using fewer files: no need for separate headers and implementation files for something so minimal)
    --HG--
    branch : lhapdfv6

--- examples/Analytic/analytic.cpp
+++ examples/Analytic/analytic.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
-#include "PDFAnalytic.h"
+#include "PDF.h"
+#include <cmath>
+#include <iostream>
 
 using namespace LHAPDF;
+using namespace std;
+
+
+/// Demo of an analytic/custom PDF class
+struct PDFAnalytic: public PDF {
+
+  double xfxQ2(PID_t id, double x, double q2) const {
+    return 0.15 * sin(20.0*x) * sin(20.0*q2); // same for all PIDs
+  }
+
+  bool inRangeQ2(double x, const double q2) const {
+    return true;
+  }
+
+  bool hasPID(double id) const {
+    return true;
+  }
+
+};
+
 
 int main( int argc, const char * argv[] ) {
-    PDFAnalytic a;
-    
-    for( double x = 0; x < 1.0; x += 0.1 ) {
-        for( double q2 = 0; q2 < 10; ++q2 ) {
-            std::cout << x << " " << q2 << " " << a.xfxQ2( 0, x, q2 ) << std::endl;
-        }
+  PDFAnalytic a;
+  for (double x = 0; x < 1.0; x += 0.1) {
+    for (double q2 = 0; q2 < 10; ++q2) {
+      cout << x << " " << q2 << " " << a.xfxQ2( 0, x, q2 ) << endl;
     }
-    
-    return 0;
+  }
+  return 0;
 }
