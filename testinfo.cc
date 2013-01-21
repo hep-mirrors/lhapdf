aff099d5aa73d61e1c7f356ba4743abdde8524c3
buckley <none@none>
2013-01-21 09:42:13 +0000

    Info test updates
    --HG--
    branch : lhapdfv6

--- testinfo.cc
+++ testinfo.cc
@@ -6,9 +6,13 @@ using namespace std;
 
 int main() {
   const LHAPDF::Info& cfg = LHAPDF::Info::config();
-  cout << cfg.metadata("Foo") << endl;
-  cout << cfg.metadata("Flavors") << endl;
-  vector<int> pids = cfg.metadata< vector<int> >("Flavors");
+  cout << cfg.metadata("ImplicitFlavorAction") << endl;
+  cout << cfg.metadata("Verbosity") << endl;
+
+  const LHAPDF::Info info("EXAMPLEPDF/EXAMPLEPDF_0000.lha");
+  cout << info.metadata("PdfDesc") << endl;
+  cout << info.metadata("PdfType") << endl;
+  vector<int> pids = info.metadata< vector<int> >("Flavors");
   foreach (int f, pids) cout << f << endl;
   cout << LHAPDF::to_str(pids) << endl;
   return 0;
