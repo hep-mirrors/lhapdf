4aff91a9b4f8e5fb717553cfb1116d4ff7f94896
buckley <none@none>
2013-01-22 19:54:26 +0000

    Doxy param tweaking
    --HG--
    branch : lhapdfv6

--- include/LHAPDF/NearestPointExtrapolator.h
+++ include/LHAPDF/NearestPointExtrapolator.h
@@ -8,12 +8,11 @@ namespace LHAPDF {
   /// Extrapolates using the closest point on the Grid.
   class NearestPointExtrapolator : public Extrapolator {
 
-    /// \param Grid the GridPDF to extrapolate
-    /// \param ID the MC Parton ID
-    /// \param X the momentum fraction
-    /// \param Q2 the squared energy scale
-    /// \return the xfx value at (X,Q2)
-    double extrapolateXQ2(int, double x, double q2 ) const;
+    /// \param id The PDG parton ID code
+    /// \param x The momentum fraction
+    /// \param q2 The squared energy scale
+    /// \return The xf value at (x,q2)
+    double extrapolateXQ2(int id, double x, double q2) const;
 
   };
 
