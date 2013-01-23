466b3450c4c81e9060fff3958593784dab785c06
buckley <none@none>
2013-01-23 20:29:15 +0000

    More tweaks
    --HG--
    branch : lhapdfv6

--- examples/analytic.cc
+++ examples/analytic.cc
@@ -1,12 +1,11 @@
 #include "LHAPDF/PDF.h"
-#include <cmath>
 #include <iostream>
 
 using namespace std;
 
 
 /// Demo of an analytic/custom PDF class
-struct PDFAnalytic : public LHAPDF::PDF {
+struct AnalyticPDF : public LHAPDF::PDF {
 
   double _xfxQ2(int id, double x, double q2) const {
     if (abs(id) > 5) return 0;
@@ -16,6 +15,7 @@ struct PDFAnalytic : public LHAPDF::PDF {
   bool inRangeX(double x) const { return true; }
   bool inRangeQ2(double q2) const { return true; }
 
+  /// @todo Something goes wrong and it throws a MetadataError... MD shouldn't be being used. Hmm.
   vector<int> flavors(double id) const {
     vector<int> rtn;
     for (int id = 1; id < 6; ++id) {
@@ -29,8 +29,9 @@ struct PDFAnalytic : public LHAPDF::PDF {
 };
 
 
-int main( int argc, const char * argv[] ) {
-  PDFAnalytic a;
+int main(int argc, const char* argv[]) {
+  AnalyticPDF a;
+  PDF& a2 = a;
   for (double x = 0; x < 1.0; x += 0.1) {
     for (double q2 = 0; q2 < 10; ++q2) {
       cout << x << " " << q2 << " " << a.xfxQ2(0, x, q2) << endl;
