6a7ff9c71c5e6bed85f6072a834331e85d408e8f
buckley <none@none>
2013-03-01 13:16:13 +0000

    Add a fallback to use LHAPATH if LHAPDF_DATA_PATH is undefined. (pre-deprecated?)
    --HG--
    branch : lhapdfv6

--- src/Paths.cc
+++ src/Paths.cc
@@ -8,13 +8,11 @@ namespace LHAPDF {
     vector<path> rtn;
     // Use LHAPDF_DATA_PATH for all path storage
     char* pathsvar = getenv("LHAPDF_DATA_PATH");
+    // But fall back to looking in LHAPATH if the preferred var is not defined
+    if (pathsvar == 0) pathsvar = getenv("LHAPATH");
+    // Split the paths variable as usual
     if (pathsvar != 0) split(rtn, pathsvar, is_any_of(":"), token_compress_on);
-    // Fall back to current directory if enabled in the central config
-    /// @todo Can;t do this, since an infinite loop appears when loading the config()!
-    // if (Info::config().has_key("PwdInSearchPath")) {
-    //   if (Info::config().metadata<bool>("PwdInSearchPath")) rtn.push_back(".");
-    // }
-    /// Fall back to install prefix
+    /// Always look in the install prefix, after other paths are exhausted
     path datadir = LHAPDF_DATA_PREFIX;
     rtn.push_back(datadir / "LHAPDF");
    return rtn;
