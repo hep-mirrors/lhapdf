1e4240ffb16304baba252c6ed48435d848507af5
mruefenacht <none@none>
2013-03-13 16:04:14 +0000

    Updated dumpv5/v6
    --HG--
    branch : lhapdfv6

--- migration/dumpv5.cc
+++ migration/dumpv5.cc
@@ -1,21 +1,54 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <math.h>
+#include <stdlib.h>
 #include <math.h>
 
 #include <LHAPDF/LHAPDF.h>
 
+#define MINLOGX -10
+#define MINLOGQ2 1
+#define MAXLOGQ2 8
+
+#define DX 0.1
+#define DQ2 0.1
+
 int main( int argc, const char* argv[] ) {
+    if( argc < 3 ) {
+        std::cerr << "./dumpv5 name member." << std::endl;
+        
+        return 1;
+    }
+
     //Use CT10 LHgrid
-    LHAPDF::initPDFSetByName( "CT10.LHgrid" );
+    const char* setname = argv[1];
+    LHAPDF::initPDFSetByName( setname );
 
     //Centeral value
-    LHAPDF::initPDF( 0 );
+    const int member = atoi( argv[2] );
+    LHAPDF::initPDF( member );
 
     //Dump out points in (X,Q2)
-    for( double x = 0.05; x <= 1.0; x += 0.05 ) {
-        for( double q2 = 1e2; q2 < 1e5; q2 += 1e2 ) {
-            std::cout << x << " " << q2 << " ";
-            std::cout << LHAPDF::xfx( x, sqrt(q2), 2 ) << std::endl;
+    for( int flavour = -6; flavour < 7; ++flavour ) {
+        std::stringstream filename;
+        filename << "flavour_v6_" << flavour << ".dat";
+
+        std::ofstream output( filename.str().c_str() );
+        for( double logX = MINLOGX; logX <= 0.0; logX += DX ) {
+            for( double logQ2 = MINLOGQ2; logQ2 <= MAXLOGQ2; logQ2 += DQ2 ) {
+                double x  = pow( 10, logX );
+                double q2 = pow( 10, logQ2 );
+
+                //std::cout << x << " " << q2 << " ";
+                //std::cout << LHAPDF::xfx( x, sqrt(q2), flavour ) << std::endl;
+
+                output << x << " " << q2 << " ";
+                output << LHAPDF::xfx(x,sqrt(q2),flavour) << std::endl;
+            }
         }
+
+        output.close();
     }
     
     return 0;
--- migration/dumpv6.cc
+++ migration/dumpv6.cc
@@ -1,17 +1,48 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 
 #include <LHAPDF/GridPDF.h>
 
+#define MINLOGX -10
+#define MINLOGQ2 1
+#define MAXLOGQ2 8
+
+#define DX 0.1
+#define DQ2 0.1
+
 int main( int argc, const char* argv[] ) {
+    if( argc < 3 ) {
+        std::cerr << "./dumpv6 setname setmember" << std::endl;
+
+        return 1;
+    }
+
     //Use CT10 PDFSet
-    const LHAPDF::GridPDF pdf( "CT10", 0 );
+    const char* setname = argv[1];
+    const int setmember = atoi( argv[2] );
+    const LHAPDF::GridPDF pdf( setname, setmember );
 
     //Dump out points in (X,Q2)
-    for( double x = 0.05; x <= 1.0; x += 0.05 ) {
-        for( double q2 = 1e2; q2 < 1e5; q2 += 1e2 ) {
-            std::cout << x << " " << q2 << " ";
-            std::cout << pdf.xfxQ2( 2, x, q2 ) << std::endl;
+    int flavours[] ={-5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 21};
+    for( int idx = 0; idx < 11; ++idx ) {
+        int flavour = flavours[idx];
+
+        std::stringstream filename;
+        filename << "flavour_v6_" << flavour << ".dat";
+
+        std::ofstream output( filename.str().c_str() );
+        for( double logX = MINLOGX; logX <= 0.0; logX += DX ) {
+            for( double logQ2 = MINLOGQ2; logQ2 <= MAXLOGQ2; logQ2 += DQ2 ) {
+                double x  = pow( 10, logX );
+                double q2 = pow( 10, logQ2 );
+
+                output << x << " " << q2 << " ";
+                output << pdf.xfxQ2(flavour, x, q2) << std::endl;
+            }
         }
+
+        output.close();
     }
 
     return 0;
