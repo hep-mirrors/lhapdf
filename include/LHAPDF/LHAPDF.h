6676f25654cba8a3c97d5bf160fff6da0f4b4c0d
buckley <none@none>
2013-06-10 07:46:58 +0000

    Adding availablePDFSets() function
    --HG--
    branch : lhapdfv6

--- include/LHAPDF/LHAPDF.h
+++ include/LHAPDF/LHAPDF.h
@@ -13,3 +13,39 @@
 #include "LHAPDF/PDFInfo.h"
 #include "LHAPDF/Factories.h"
 #include "LHAPDF/PDFIndex.h"
+#include "LHAPDF/Paths.h"
+
+namespace LHAPDF {
+
+
+  /// @name Global config metadata specialisations
+  //@{
+
+  /// @brief All available PDF set names
+  ///
+  /// @note Taken from scanning the directories in LHAPDF_DATA_PATH for
+  /// viable PDF sets.
+  const std::vector<std::string>& availablePDFSets() {
+    static vector<string> rtn;
+    if (rtn.empty()) {
+      foreach (const path& p, paths()) {
+        if (exists(p) && is_directory(p)) {
+          directory_iterator idir(p), iend;
+          while (idir != iend) {
+            const path infopath = idir->path() / (idir->path().filename().string() + ".info");
+            if (is_directory(*idir) && exists(infopath)) {
+              rtn.push_back(idir->path().filename().string());
+            }
+            idir++;
+          }
+        }
+      }
+      sort(rtn.begin(), rtn.end());
+    }
+    return rtn;
+  }
+
+  //@}
+
+
+}
--- tests/testpaths.cc
+++ tests/testpaths.cc
@@ -1,8 +1,8 @@
 // Test program for path searching machinery
 
+#include "LHAPDF/LHAPDF.h"
 #include "LHAPDF/Paths.h"
 #include <iostream>
-
 using namespace std;
 
 int main() {
@@ -10,5 +10,11 @@ int main() {
     cout << p << endl;
   }
   cout << "@" << LHAPDF::findFile("lhapdf.conf") << "@" << endl;
+
+  cout << "List of available PDFs:" << endl;
+  foreach (const string& s, LHAPDF::availablePDFSets()) {
+    cout << " " << s << endl;
+  }
+
   return 0;
 }
