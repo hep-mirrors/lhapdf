31c8301b2e3c819569c1fbbc9cb2057c934b6062
buckley <none@none>
2013-07-03 15:29:17 +0000

    Adding a test program for member switching, and better timing in the test*perf programs
    --HG--
    branch : lhapdfv6

--- tests/testsetperf.cc
+++ tests/testsetperf.cc
@@ -11,27 +11,29 @@ int main(int argc, char* argv[]) {
   const double MINLOGX = -7.5;
   const double MINLOGQ = 1;
   const double MAXLOGQ = 3;
-  const double dx = 0.005;
-  const double dq = 0.005;
+  const double dx = 0.01;
+  const double dq = 0.01;
 
   const clock_t start = clock();
 
   #if LHAPDF_MAJOR_VERSION > 5
-  const LHAPDF::PDF* pdf = LHAPDF::mkPDF("CT10nlo", 0);
-  #define XFS(X, Q) pdf->xfxQ(X, Q, xfs)
+  const vector<LHAPDF::PDF*> pdfs = LHAPDF::mkPDFs("CT10nlo");
+  #define XFS(I, X, Q) pdfs[I]->xfxQ(X, Q, xfs)
   #else
   LHAPDF::initPDFSetByName("CT10nlo.LHgrid");
   LHAPDF::initPDF(0);
-  #define XFS(X, Q) LHAPDF::xfx(X, Q, &xfs[0])
+  #define XFS(I, X, Q) { LHAPDF::initPDF(I+1); LHAPDF::xfx(X, Q, &xfs[0]); }
   #endif
 
   const clock_t init = clock();
 
-  /// Tests of a single PDF member
+  /// Tests of switchng between members of a PDF set
   vector<double> xfs; xfs.resize(13);
   for (double log10x = MINLOGX; log10x <= 0.0; log10x += dx) {
     for (double log10q = MINLOGQ; log10q <= MAXLOGQ; log10q += dq) {
-      XFS(pow(10, log10x), pow(10, log10q));
+      for (int i = 1; i < 52; ++i) {
+        XFS(i, pow(10, log10x), pow(10, log10q));
+      }
     }
   }
 
