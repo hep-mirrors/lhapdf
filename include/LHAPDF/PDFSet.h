a0a48f47029f042ee4284dda739b1be8ced3f796
Andy Buckley <andy@insectnation.org>
2014-03-23 19:59:50 +0100

    Moving new uncertainty functions into a new PDFSet.cc file to keep header clean.
    --HG--
    branch : lhapdfv6

--- include/LHAPDF/PDFSet.h
+++ include/LHAPDF/PDFSet.h
@@ -9,8 +9,6 @@
 #include "LHAPDF/Factories.h"
 #include "LHAPDF/Version.h"
 #include "LHAPDF/Config.h"
-/// @todo Keep this to the .cc files?
-#include <boost/math/distributions/chi_squared.hpp>
 
 namespace LHAPDF {
 
@@ -32,21 +30,7 @@ namespace LHAPDF {
 
     /// Constructor from a set name
     /// @todo Remove?
-    PDFSet(const std::string& setname) {
-      _setname = setname;
-      const string setinfopath = findpdfsetinfopath(setname);
-      if (!file_exists(setinfopath))
-        throw ReadError("Data file not found for PDF set '" + setname + "'");
-      // /// Print out a banner if sufficient verbosity is enabled
-      // const int verbosity = get_entry_as<int>("Verbosity", 1);
-      // if (verbosity > 0) {
-      //   std::cout << "Loading PDF set '" << setname << "'" << std::endl;
-      //   print(std::cout, verbosity);
-      // }
-      // Load info file
-      load(setinfopath);
-      /// @todo Check that some mandatory metadata keys have been set: _check() function.
-    }
+    PDFSet(const std::string& setname);
 
     //@}
 
@@ -84,9 +68,7 @@ namespace LHAPDF {
     /// @brief Get the confidence level of the Hessian eigenvectors, in percent.
     ///
     /// If not defined, assume 1-sigma = erf(1/sqrt(2)) = 68.268949% by default.
-    double errorConfLevel() const {
-      return get_entry_as<double>("ErrorConfLevel", 100*boost::math::erf(1/sqrt(2)));
-    }
+    double errorConfLevel() const;
 
     /// Number of members in this set
     // int numMembers() const {
@@ -100,19 +82,11 @@ namespace LHAPDF {
 
 
     /// Summary printout
-    void print(std::ostream& os=std::cout, int verbosity=1) const {
-      std::stringstream ss;
-      if (verbosity > 0)
-        ss << name() << ", version " << dataversion() << "; " << size() << " PDF members";
-      if (verbosity > 1)
-        ss << "\n" << description();
-      os << ss.str() << std::endl;
-    }
+    void print(std::ostream& os=std::cout, int verbosity=1) const;
 
 
     /// @name Creating PDF members
     //@{
-
 
     /// Make the nth PDF member in this set, returned by pointer
     ///
@@ -144,6 +118,8 @@ namespace LHAPDF {
     /// they also need to be compatible with storage in STL containers. This is *not*
     /// the case with std::auto_ptr (which for this reason is replaced with
     /// std::unique_ptr in C++11).
+    //
+    /// @todo Needs to be implemented in the header since the arg type is templated.
     template <typename PTR>
     void mkPDFs(std::vector<PTR>& pdfs) const {
       const int v = verbosity();
@@ -220,231 +196,23 @@ namespace LHAPDF {
     /// for quantities with a non-linear dependence on PDFs.  In the Hessian approach,
     /// the central value is the best-fit "values[0]" and the uncertainty is given by either
     /// the symmetric or asymmetric formula using eigenvector PDF sets.
-    std::vector<double> uncertainty(const std::vector<double>& values) const {
-      if (values.size() != size())
-        throw UserError("Error in LHAPDF::PDFSet::uncertainty. Input vector must contain values for all PDF members.");
-
-      // The following five values will be returned in a vector<double>.
-      double central = values[0], errplus = 0.0, errminus = 0.0, errsym = 0.0, scale = 1.0;
-      const int nmem = size()-1;
-
-      if (errorType() == "replicas") {
-
-        // Calculate the average and standard deviation using Eqs. (2.3) and (2.4) of arXiv:1106.5788v2.
-        double av = 0.0, sd = 0.0;
-        for (int imem = 1; imem <= nmem; imem++) {
-          av += values[imem];
-          sd += sqr(values[imem]);
-        }
-        av /= nmem; sd /= nmem;
-        sd = nmem/(nmem-1.0)*(sd-av*av);
-        if (sd > 0.0 && nmem > 1) sd = sqrt(sd);
-        else sd = 0.0;
-        central = av;
-        errplus = errminus = errsym = sd;
-
-      } else if (errorType() == "symmhessian") {
-
-        for (int ieigen = 1; ieigen <= nmem; ieigen++) {
-          errsym += sqr(values[ieigen]-values[0]);
-        }
-        errsym = sqrt(errsym);
-        errplus = errsym;
-        errminus = errsym;
-
-      } else if (errorType() == "hessian") {
-
-        // Calculate the asymmetric and symmetric Hessian uncertainties
-        // using Eqs. (2.1), (2.2) and (2.6) of arXiv:1106.5788v2.
-        for (int ieigen = 1; ieigen <= nmem/2; ieigen++) {
-          errplus += sqr(max(max(values[2*ieigen-1]-values[0],values[2*ieigen]-values[0]),0.0));
-          errminus += sqr(max(max(values[0]-values[2*ieigen-1],values[0]-values[2*ieigen]),0.0));
-          errsym += sqr(values[2*ieigen-1]-values[2*ieigen]);
-        }
-        errsym = 0.5*sqrt(errsym);
-        errplus = sqrt(errplus);
-        errminus = sqrt(errminus);
-
-      } else {
-
-        throw MetadataError("\"ErrorType: " + errorType() + "\" not supported by LHAPDF::PDFSet::uncertainty.");
-
-      }
-
-      // Return central value and asymmetric/symmetric PDF uncertainties as a vector<double>
-      std::vector<double> err;
-      err.push_back(central);
-      err.push_back(errplus);
-      err.push_back(errminus);
-      err.push_back(errsym);
-      err.push_back(scale);
-      return err;
-    }
-
+    std::vector<double> uncertainty(const std::vector<double>& values) const;
 
     /// @brief Calculate central value and error using vector @c values and optional args for stat treatment
     ///
     /// Optional argument @c inputCL to rescale uncertainties to a particular confidence level.
     /// If the PDF set is given in the form of replicas, then optional argument @c median will
     /// instead calculate the median and confidence interval of the probability distribution.
-    std::vector<double> uncertainty(const std::vector<double>& values, double inputCL, bool median=false) const {
+    std::vector<double> uncertainty(const std::vector<double>& values, double inputCL, bool median=false) const;
 
-      double scale = 1.0;
-      std::vector<double> err = uncertainty(values);
-
-      double reqCL = inputCL / 100.0; // convert from percentage
-      double errCL = errorConfLevel() / 100.0; // convert from percentage
-
-      /// Check that reqCl and errCL both lie between 0 and 1.
-      if (reqCL < 0 || reqCL > 1 || errCL < 0 || errCL > 1)
-        return err;
-
-      if (errorType() == "replicas" && median) {
-
-        /// Compute median and requested C.L. directly from probability distribution of replicas.
-
-        /// Sort "values" into increasing order, ignoring zeroth member (average over replicas).
-        std::vector<double> sorted = values;
-        std::sort(sorted.begin()+1,sorted.end());
-        const int nmem = size()-1;
-        /// Define central value to be median.
-        if (nmem % 2) { // odd nmem => one middle value
-          err[0] = sorted[nmem/2+1];
-        } else { // even nmem => average of two middle values
-          err[0] = 0.5*(sorted[nmem/2] + sorted[nmem/2+1]);
-        }
-        /// Define uncertainties with a C.L. given by "reqCL".
-        int upper = round(0.5*(1+reqCL)*nmem); // round to nearest integer
-        int lower = 1+round(0.5*(1-reqCL)*nmem); // round to nearest integer
-        err[1] = sorted[upper] - err[0];
-        err[2] = err[0] - sorted[lower];
-        err[3] = 0.5*(err[1] + err[2]); // symmetrised
-
-      } else {
-
-        /// Calculate the qth quantile of the chi-squared distribution with one degree of freedom.
-        /// Examples: quantile(dist, q) = {0.988946, 1, 2.70554, 3.84146, 4} for q = {0.68, 1-sigma, 0.90, 0.95, 2-sigma}.
-        boost::math::chi_squared dist(1);
-        double qerrCL = boost::math::quantile(dist,errCL);
-        double qreqCL = boost::math::quantile(dist,reqCL);
-        scale = sqrt(qreqCL/qerrCL);
-
-        for (int i = 1; i <= 3; i++) {
-          err[i] *= scale;
-        }
-        err[4] = scale;
-      }
-
-      return err;
-
-    }
-
-
-    /// Calculate the PDF correlation using appropriate formulae given vectors "valuesA" and "valuesB".
+    /// @brief Calculate the PDF correlation using appropriate formulae given vectors "valuesA" and "valuesB".
+    ///
     /// The correlation can vary between -1 and +1 where values close to {-1,0,+1} mean that the two
     /// quantities A and B are {anticorrelated,uncorrelated,correlated}, respectively.
-    double correlation(const vector<double>& valuesA, const vector<double>& valuesB) const {
-
-      if (valuesA.size() != size() || valuesB.size() != size()) {
-        throw UserError("Error in LHAPDF::PDFSet::correlation.  Input vectors must contain values for all PDF members.");
-      }
-
-      double cor = 0.0;
-
-      std::vector<double> errA = uncertainty(valuesA);
-      std::vector<double> errB = uncertainty(valuesB);
-      const int nmem = size()-1;
-
-      if (errorType() == "replicas" && nmem > 1) {
-
-        /// Calculate the correlation using Eq. (2.7) of arXiv:1106.5788v2.
-
-        for (int imem = 1; imem <= nmem; imem++) {
-          cor += valuesA[imem]*valuesB[imem];
-        }
-        cor = (cor/nmem - errA[0]*errB[0])/(errA[3]*errB[3]) * nmem/(nmem-1.0);
-
-      } else if (errorType() == "symmhessian") {
-
-        for (int ieigen = 1; ieigen <= nmem; ieigen++) {
-          cor += (valuesA[ieigen]-errA[0])*(valuesB[ieigen]-errB[0]);
-        }
-        cor /= errA[3]*errB[3];
-
-      } else if (errorType() == "hessian") {
-
-        /// Calculate the correlation using Eq. (2.5) of arXiv:1106.5788v2.
-
-        for (int ieigen = 1; ieigen <= nmem/2; ieigen++) {
-          cor += (valuesA[2*ieigen-1]-valuesA[2*ieigen])*(valuesB[2*ieigen-1]-valuesB[2*ieigen]);
-        }
-        cor /= 4.0*errA[3]*errB[3];
-
-      }
-
-      return cor;
-
-    }
-
+    double correlation(const vector<double>& valuesA, const vector<double>& valuesB) const;
 
     /// Generate a random value from Hessian "values" and Gaussian random numbers.
-    double randomValue(const vector<double>& values, const vector<double>& random, bool symmetrise=true) const {
-
-      if (values.size() != size()) {
-        throw UserError("Error in LHAPDF::PDFSet::randomValue.  Input vector must contain values for all PDF members.");
-      }
-
-      double frand = 0.0;
-
-      double sigma = 100*boost::math::erf(1/sqrt(2));
-      std::vector<double> err = uncertainty(values, sigma);
-      double scale = err[4];
-      int nmem = size()-1;
-
-      /// Allocate number of eigenvectors based on ErrorType.
-      unsigned neigen = 0;
-      if (errorType() == "hessian") {
-        neigen = nmem/2;
-      } else if (errorType() == "symmhessian") {
-        neigen = nmem;
-      } else {
-        throw UserError("Error in LHAPDF::PDFSet::randomValue.  This PDF set is not in the Hessian format.");
-      }
-
-      if (random.size() != neigen) {
-        throw UserError("Error in LHAPDF::PDFSet::randomValue.  Input vector must contain random numbers for all eigenvectors.");
-      }
-
-      if (errorType() == "symmhessian") {
-
-        frand = values[0];
-        /// Loop over number of eigenvectors.
-        for (unsigned ieigen = 1; ieigen <= neigen; ieigen++) {
-          double r = random[ieigen-1]; // Gaussian random number
-          frand += r*abs(values[ieigen]-values[0])*scale;
-        }
-
-      } else if (errorType() == "hessian") {
-
-        /// Use either Eq. (6.4) or Eq. (6.5) of arXiv:1205.4024v2.
-
-        frand = values[0];
-        /// Loop over number of eigenvectors.
-        for (unsigned ieigen = 1; ieigen <= neigen; ieigen++) {
-          double r = random[ieigen-1]; // Gaussian random number
-          if (symmetrise) {
-            frand += 0.5*r*abs(values[2*ieigen-1]-values[2*ieigen])*scale;
-          } else { // not symmetrised
-            if (r<0.0) frand -= r*(values[2*ieigen]-values[0])*scale; // negative direction
-            else frand += r*(values[2*ieigen-1]-values[0])*scale; // positive direction
-          }
-        }
-
-      }
-
-      return frand;
-
-    }
+    double randomValue(const vector<double>& values, const vector<double>& random, bool symmetrise=true) const;
 
     //@}
 
